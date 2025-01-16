259acca8c1041b3cf32483a63496976977cda069
Aditya Sharma <aadisharma.in+github@gmail.com>
2025-01-16 18:40:24 +0530

    asset fix

--- src/components/chat-list.tsx
+++ src/components/chat-list.tsx
@@ -3,10 +3,9 @@ import React from "react";
 import Markdown from "react-markdown";
 //  @ts-ignore
 import codeBlock from 'remark-code-blocks'
-import capitalizeHeadings from 'remark-capitalize-headings'
 import { Roboto } from "next/font/google";
-import "@/highlight/styles/agate.css";
 import rehypeHighlight from "rehype-highlight";
+import "@/highlight/styles/agate.css";
 
 const roboto = Roboto({
     weight: "400",
@@ -40,7 +39,7 @@ export default function ChatList({ chats }: { chats: string[] }) {
           return (
             <div className="flex justify-end font-light" key={index}>
               <div className="bg-neutral-900 w-full md:max-w-[70%] rounded-xl px-6 py-4">
-                <Markdown rehypePlugins={[rehypeHighlight]} className={'text-neutral-300 overflow-x-scroll'} remarkPlugins={[codeBlock, capitalizeHeadings]} >{chat}</Markdown>
+                <Markdown rehypePlugins={[rehypeHighlight]} className={'text-neutral-300 overflow-x-scroll'} remarkPlugins={[codeBlock]} >{chat}</Markdown>
               </div>
             </div>
           );
